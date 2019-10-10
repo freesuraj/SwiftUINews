ffabd817f526adecfe4020bba929b65ffb0b65f4
Suraj Pathak <freesuraj@gmail.com>
2019-10-11 10:22:41 +1100

    Initial Commit

---
+++ SwiftUINews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINews
+//
+//  Created by Suraj Pathak on 11/10/19.
+//  Copyright © 2019 Laohan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
